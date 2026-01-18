ec1a06caf563e42c69e5ed037f2eba17f629b148
ian <paletaian56@gmail.com>
2026-01-18 18:19:35 -0300

    feat: keyboard control design

--- app/pages/controller.tsx
+++ app/pages/controller.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import { Alert } from 'react-native';
 import { themes } from '../styles/themes';
 
@@ -10,8 +10,13 @@ import { useTcp } from '../contexts/tcpContext';
 import { useEncryptionKey, useHmacKey } from '../contexts/secureKeyContext';
 import { buildMessage } from '../protocols/messageMaster';
 
-//@ts-expect-error
-import Icon from 'react-native-vector-icons/Ionicons';
+import { MaterialDesignIcons } from '@react-native-vector-icons/material-design-icons';
+//@ts-ignore
+import IonIcon from 'react-native-vector-icons/Ionicons';
+//@ts-ignore
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+//@ts-ignore
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 
 
@@ -22,6 +27,9 @@ export const Controller: React.FC = () => {
     
     const encryption_key_ref = useEncryptionKey(); 
     const hmac_key_ref = useHmacKey();
+
+    const [command, setCommand] = useState<"none"|"keyboard"|"mouse"|"load">("none");
+    const [keyboardInput, setKeyboardInput] = useState<string>("");
 
 
     const sendControlSignal = (signal: string) => {
@@ -35,9 +43,34 @@ export const Controller: React.FC = () => {
 
     return (
         <View style={style_sheet.container}>
-            <TouchableOpacity style={style_sheet.powerButton} onPress={() => sendControlSignal('POWER_TOGGLE')}>
-                <Icon name="power" size={100} color={themes.default.primary}/>
-            </TouchableOpacity>
+            {command === "none" && (<>
+                <TouchableOpacity style={style_sheet.power_button} onPress={() => sendControlSignal('POWER_TOGGLE')}>
+                    <IonIcon name="power" size={70} color={themes.default.primary}/>
+                </TouchableOpacity>
+                <View>
+                    <TouchableOpacity style={{...style_sheet.generic_button, marginBottom: 30}} onPress={() => setCommand("keyboard")}>
+                        <Text style={style_sheet.button_text} onPress={() => setCommand("keyboard")}>Keyboard</Text>
+                        <EntypoIcon name="keyboard" size={40} color={themes.default.primary} style={style_sheet.button_icon}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={style_sheet.generic_button} onPress={() => setCommand("mouse")}>
+                        <Text style={style_sheet.button_text} onPress={() => setCommand("mouse")}>Mouse</Text>
+                        <MaterialIcon name="mouse" size={40} color={themes.default.primary} style={style_sheet.button_icon}/>
+                    </TouchableOpacity>
+                </View>
+            </>)}
+            {command === "keyboard" && (
+                <View style={style_sheet.text_input_container}>
+                    <TextInput style={style_sheet.text_input} onChangeText={setKeyboardInput} value={keyboardInput}></TextInput>
+                    <View style={{flexDirection: 'row', justifyContent: 'center', width: '80%'}}>
+                        <TouchableOpacity style={{...style_sheet.generic_button, width: '40%', marginTop: 30, marginRight: 10}} onPress={() => {}}>
+                            <MaterialDesignIcons name="keyboard-backspace" size={30} color={themes.default.primary} style={style_sheet.button_icon}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{...style_sheet.generic_button, width: '40%', marginTop: 30}} onPress={() => {}}>
+                            <MaterialDesignIcons name="keyboard-return" size={30} color={themes.default.primary} style={style_sheet.button_icon}/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )}
         </View>
     );
 };
@@ -48,15 +81,57 @@ const style_sheet = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'space-evenly',
     },
-    powerButton: {
-        width: 150,
-        height: 150,
+    power_button: {
+        width: 100,
+        height: 100,
         borderRadius: 100,
         backgroundColor: "red",
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    generic_button_container: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+    },
+    generic_button: {
+        position: 'relative',
+        width: '80%',
+        height: 60,
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: themes['default'].secondary,
+    },
+    button_text: {
+        color: themes.default.primary,
+        fontSize: 16,
+        flexGrow: 1,
+        textAlign: 'center',
+    },
+    button_icon: {
+        position: 'absolute',
+        right: 10,
+    },
+    text_input_container: {
+        width: '100%',
+        marginTop: 50,
+        flex: 1,
+        alignItems: 'center',
+    },
+    text_input: {
+        width: '80%',
+        height: 50,
+        borderColor: themes.default.secondary,
+        borderWidth: 3,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        color: themes.default.primary,
     },
 });
 
